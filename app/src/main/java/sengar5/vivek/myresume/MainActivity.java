61b874d80f564967603adaf0f566411f13b84124
Viveksengar07 <vivek.sengar_mca18@gla.ac.in>
2019-01-16 15:58:52 +0530

    Initial commit

---
+++ app/src/main/java/sengar5/vivek/myresume/MainActivity.java
@@ -0,0 +1,13 @@
+package sengar5.vivek.myresume;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
